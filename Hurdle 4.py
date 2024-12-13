f3f3ab2b8de742ba917ea312298cc01fe620ca0b
Tiara Williams <42846282+twill4908@users.noreply.github.com>
2024-12-13 11:51:36 -0500

    Add files via upload

--- Hurdle 4.py
+++ Hurdle 4.py
@@ -1,4 +1,29 @@
-move()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+    
+def jump():
+    turn_left()
+    #going upward
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    #going downward
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+        
+    
 ################################################################
 # WARNING: Do not change this comment.
 # Library Code is below.
